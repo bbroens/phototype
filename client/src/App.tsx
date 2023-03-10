e55b8156afa8ecffdd3b581edda1d1b4e4cd0d24
Ben Broens <ben@shapeinteractive.nl>
2023-03-11 00:08:00 +0100

    react router structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,42 @@
 import React from "react";
 import "./app.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 const App = () => {
-  return <div className="App">Hello, world!</div>;
+  const AppLayout = () => {
+    return (
+      <div>
+        header
+        <div>
+          menu
+          <div>
+            <Outlet />
+          </div>
+          side menu
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return <main>home</main>;
+};
+
+export default Home;
