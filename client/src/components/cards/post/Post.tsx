d1227826a1e95b52fa2be3b9d68b54f9ca595c6f
Ben Broens <ben@shapeinteractive.nl>
2023-03-11 19:15:00 +0100

    updated posts to support multiple images

--- client/src/components/cards/post/Post.tsx
+++ client/src/components/cards/post/Post.tsx
@@ -1,9 +1,10 @@
 import "./post.scss";
 import { useState } from "react";
 import Comments from "../../comments/Comments";
+import FavoriteOutlineIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import CommentIcon from "@mui/icons-material/Comment";
 import { Link } from "react-router-dom";
-
 export interface IPost {
   post_id: number;
   name: string;
@@ -19,7 +20,27 @@ type Props = {
 };
 
 export const Post = ({ postObj }: Props) => {
-  //TODO Get images from array
+  const [commentOpen, setCommentOpen] = useState(false);
+
+  //TODO Get from db
+  const liked = false;
+
+  // Return either collage or a single wide photo
+  const processedImages = (images: Array<string>) => {
+    if (images.length > 1) {
+      return (
+        <div className="collage">
+          {images.map((image, index) => (
+            <div key={index} className="collage-photo">
+              <img alt="Photo" src={`/${image}`} />
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return <img src={`/${images[0]}`} alt="Photograph" />;
+  };
+
   return (
     <div className="postCard">
       <div className="postContainer">
@@ -31,16 +52,30 @@ export const Post = ({ postObj }: Props) => {
           <span className="timePosted">Posted recently</span>
         </div>
         <div className="postBody">
-          <div className="shots">images</div>
+          <div className="shots">{processedImages(postObj.img)}</div>
           <div className="text">{postObj.text}</div>
         </div>
         <div className="postActions">
-          <button className="submitButton">liked?</button>
           <button className="submitButton">
+            {liked ? (
+              <>
+                <FavoriteIcon /> Unlike
+              </>
+            ) : (
+              <>
+                <FavoriteOutlineIcon /> Like
+              </>
+            )}
+          </button>
+          <button
+            className="submitButton"
+            onClick={() => setCommentOpen(!commentOpen)}
+          >
             <CommentIcon />
-            Show comments
+            {commentOpen ? "Hide comments" : "Show comments"}
           </button>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   );
